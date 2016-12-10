de7289077c186150a1eeabc47c3403574fd7e11a
Jordan Cech <jordancech@Jordans-MBP-2.casacisco.local>
2016-12-10 17:20:13 -0500

    Successfully completed basic email authentication w/ Firebase.

--- Nite-Owl_2.0/SignInVC.swift
+++ Nite-Owl_2.0/SignInVC.swift
@@ -15,6 +15,9 @@ import FacebookCore
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailTxtField: FancyField!
+    @IBOutlet weak var passwordTxtField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,7 +42,34 @@ class SignInVC: UIViewController {
 }
   
 //-----------------------------------------------------------------
-    
+    @IBAction func signInBtnTapped(_ sender: Any) {
+        
+        guard let email = emailTxtField.text, email.characters.count > 0 && isValidEmail(testStr: email) else {
+            print("Please enter valid email address.")
+            return
+        }
+        
+        guard let password = passwordTxtField.text, password.characters.count >= 6 else {
+            print("Please enter a password at least 6 characters long.")
+            return
+        }
+        
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+            if error == nil {
+                print("Successfully Signed In w/ Email.")
+            } else {
+                FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                    if error != nil {
+                        print("JAY: Unable to authenticate w/ Firebase using email.")
+                    } else {
+                        print("JAY: Successfully REGISTERED w/ email!")
+                    }
+                })
+            }
+        })
+        
+    }
+//-----------------------------------------------------------------
     // 2. Authenticate with Firebase.
     func firebaseAuth(_ credential: FIRAuthCredential) {
         FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
@@ -51,5 +81,16 @@ class SignInVC: UIViewController {
             }
         })
     }
+//-----------------------------------------------------------------
+    
+    func isValidEmail(testStr: String) -> Bool {
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    
+    }
+    
 //-----------------------------------------------------------------
 }
